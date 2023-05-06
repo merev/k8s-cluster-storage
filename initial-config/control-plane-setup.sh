65b9e3303644d73defe09ba6f344c77fc9f436ac
merev <merev.personal@gmail.com>
2023-05-06 22:42:00 +0300

    calico network plugin commands updated

--- initial-config/control-plane-setup.sh
+++ initial-config/control-plane-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "* Cluster Initialization ..."
-kubeadm init --apiserver-advertise-address=192.168.1.131 --pod-network-cidr 10.244.0.0/16 --token abcdef.0123456789abcdef
+kubeadm init --apiserver-advertise-address=192.168.1.131 --pod-network-cidr=10.244.0.0/16 --token abcdef.0123456789abcdef
 
 echo "* Save the hash to a file ..."
 openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //' > /shared/.data/hash.txt
@@ -17,7 +17,7 @@ cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config
 chown -R vagrant:vagrant /home/vagrant/.kube
 
 echo "* Save the admin config in a file ..."
-cp -i /etc/kubernetes/admin.conf /shared/.data/admin-config
+cp -i /etc/kubernetes/admin.conf /shared/.data/admin-config.yaml
 
 # Pod Network plugin - comment/uncomment one of the following blocks
 # Currently set to Calico
@@ -30,7 +30,7 @@ cp -i /etc/kubernetes/admin.conf /shared/.data/admin-config
 # kubectl apply -f /tmp/kube-flannel.yaml
 
 echo "* Install Pod Network plugin (Calico) ..."
-kubectl create -f https://docs.projectcalico.org/manifests/tigera-operator.yaml
-wget -q https://docs.projectcalico.org/manifests/custom-resources.yaml -O /tmp/custom-resources.yaml
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.1/manifests/tigera-operator.yaml
+wget -q https://raw.githubusercontent.com/projectcalico/calico/v3.25.1/manifests/custom-resources.yaml -O /tmp/custom-resources.yaml
 sed -i 's/192.168.0.0/10.244.0.0/g' /tmp/custom-resources.yaml
 kubectl create -f /tmp/custom-resources.yaml
--- initial-config/worker-setup.sh
+++ initial-config/worker-setup.sh
@@ -5,10 +5,10 @@ kubeadm join 192.168.1.131:6443 --token abcdef.0123456789abcdef --discovery-toke
 
 echo "* Copy configuration for root ..."
 mkdir -p /root/.kube
-cp -i /shared/.data/admin-config /root/.kube/config
+cp -i /shared/.data/admin-config.yaml /root/.kube/config
 chown -R root:root /root/.kube
 
 echo "* Copy configuration for vagrant ..."
 mkdir -p /home/vagrant/.kube
-cp -i /shared/.data/admin-config /home/vagrant/.kube/config
+cp -i /shared/.data/admin-config.yaml /home/vagrant/.kube/config
 chown -R vagrant:vagrant /home/vagrant/.kube
